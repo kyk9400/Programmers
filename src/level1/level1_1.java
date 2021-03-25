a72e270f1a55affca39f61119724b9defd2cfa61
kyk9400 <kyk9400@nate.com>
2021-03-25 17:49:01 +0900

    level1_1 숫자 두 개 뽑아서 더하기

--- src/level1/level1_1.java
+++ src/level1/level1_1.java
@@ -9,29 +9,28 @@ public class level1_1 {
         Solution sol = new Solution();
         int [] numberArr = {12,31,2,5,10};
         sol.solution(numberArr);
-
-
     }
-}
-class Solution {
-    public int[] solution(int[] numbers) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int i=0;i<numbers.length;i++){
-            for(int j=0;j<numbers.length;j++){
-                if(j==i){
-                    continue;
+    static class Solution {
+        public int[] solution(int[] numbers) {
+            HashSet<Integer> set = new HashSet<>();
+            for(int i=0;i<numbers.length;i++){
+                for(int j=0;j<numbers.length;j++){
+                    if(j==i){
+                        continue;
+                    }
+                    set.add(numbers[j]+numbers[i]);
                 }
-                set.add(numbers[j]+numbers[i]);
             }
-        }
 
-        int count=0;
-        int [] ret = new int[set.size()];
-        for(Integer val : set){
-            ret[count++]=val;
-        }
-        Arrays.sort(ret);
-        return ret;
+            int count=0;
+            int [] ret = new int[set.size()];
+            for(Integer val : set){
+                ret[count++]=val;
+            }
+            Arrays.sort(ret);
+            return ret;
 
+        }
     }
+
 }
